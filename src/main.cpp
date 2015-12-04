f17f5bc0c520943b486c1a60515ba72b4378e879
ZealotOfLuna <gladdensr@gmail.com>
2015-12-04 10:30:56 -0500

    working on h files and base XML parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream.h>
-#include "dungeon_object.h"
+#include <iostream>
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+#include "rapidxml_print.hpp"
+
+using namespace std;
 
 int main(){
+    rapidxml::file<> xmlFile("sample.txt.xml");
+    rapidxml::xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+    cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
+    rapidxml::xml_node<> *node = doc.first_node("map");
+    cout << "Node map has value " << node->value() << "\n";
+    
     return EXIT_SUCCESS;
 }
---
+++ src/room.hpp
@@ -0,0 +1,12 @@
+class Room{
+    private:
+        char name,
+        char status,
+        char type;
+        char description;
+        vector<Room*> border;
+        vector<Container> container;
+        vector<Item> item;
+        vector<Creature> creature;
+        trigger[]
+}
